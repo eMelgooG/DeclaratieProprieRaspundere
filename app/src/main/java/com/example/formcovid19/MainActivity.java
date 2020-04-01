c1d007fb988d11dbac5d77e00e5c4ae43096b67f
Emanuel Adrian Lucaci <manu_bc2004@hotmail.com>
2020-04-02 00:24:40 +0300

    Made the first part of the pdf

--- app/src/main/java/com/example/formcovid19/MainActivity.java
+++ app/src/main/java/com/example/formcovid19/MainActivity.java
@@ -6,7 +6,12 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -19,12 +24,19 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Constants
+    private static final String TITLU_PDF = "DECLARAȚIE PE PROPRIE RĂSPUNDERE";
+    private static final String DATA_NASTERII_PDF = "Data nașterii: ";
+    private static final String NUME_PDF = "Nume, prenume: ";
+    private static final String ADRESA_PDF = "Adresa locuinței: ";
+    private static final String ADRESA_HELPER_PDF = "Se va completa adresa locuinței în care persoana locuiește în fapt, indiferent dacă este indentică sau nu cu cea menționată în actul de identitate.";
     //Properties
     TextInputLayout dataTF;
      Button motiveDeplasareButon, generarePdfButon;
@@ -61,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
                         generarePdfButon.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
+                                generatePdf("Lucaci Emanuel","06/01/1993","Strada Vasile Alecsandri, nr. 2");
                             }
                         });
                     }
@@ -131,5 +144,78 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    private void generatePdf(String name, String birthDate, String address) {
+        PdfDocument myPdfDocument = new PdfDocument();
+        PdfDocument.PageInfo myPageInfo = new PdfDocument.PageInfo.Builder(595,842,1).create();
+        PdfDocument.Page myPage = myPdfDocument.startPage(myPageInfo);
+
+        //titlu
+        Paint titluPaint = new Paint();
+        titluPaint.setTypeface(Typeface.SERIF);
+        titluPaint.setTextSize(10.5f);
+        titluPaint.setTextAlign(Paint.Align.CENTER);
+
+        //text normal
+        Paint textNormalPaint = new Paint();
+        textNormalPaint.setTextSize(9f);
+
+        //text bold
+        Paint textBoldPaint = new Paint();
+        textBoldPaint.setTextSize(10f);
+        textBoldPaint.setFakeBoldText(true);
+
+        //Helper text
+        Paint helperTextPaint = new Paint();
+        helperTextPaint.setTextScaleX(1.4f);
+        helperTextPaint.setTextSize(4.7f);
+
+
+
+        Canvas canvas = myPage.getCanvas();
+        float y = 80;
+        float x = 80;
+
+        //draw title
+        canvas.drawText(TITLU_PDF,myPageInfo.getPageWidth()/2,y,titluPaint);
+        y = breakLine(y,titluPaint,3);
+
+        //draw name, birth date, address
+        canvas.drawText(NUME_PDF,x,y,textNormalPaint);
+        canvas.drawText(name,x+textNormalPaint.measureText(NUME_PDF),y,textBoldPaint);
+        y=breakLine(y,textNormalPaint,1.3f);
+
+        canvas.drawText(DATA_NASTERII_PDF,x,y,textNormalPaint);
+        canvas.drawText(birthDate,x+textNormalPaint.measureText(DATA_NASTERII_PDF),y,textBoldPaint);
+        y=breakLine(y,textNormalPaint,1.3f);
+
+        canvas.drawText(ADRESA_PDF,x,y,textNormalPaint);
+        canvas.drawText(address,x+textNormalPaint.measureText(ADRESA_PDF),y,textBoldPaint);
+        y=breakLine(y,textNormalPaint,1f);
+        //Helper pentru adresa
+        canvas.drawText(ADRESA_HELPER_PDF,x,y,helperTextPaint);
+
+
+        //finish document
+        myPdfDocument.finishPage(myPage);
+
+        //Create the file
+        String filePath = Environment.getExternalStorageDirectory().getPath() + "/myFile.pdf";
+        File myFile = new File(filePath);
+
+        //write to outputstream
+        try {
+            myPdfDocument.writeTo(new FileOutputStream(myFile));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        myPdfDocument.close();
+    }
+
+
+    private float breakLine(float y, Paint paint,float howMany){
+        return y+=(paint.descent()-paint.ascent())*howMany;
+    }
+
 
 }
